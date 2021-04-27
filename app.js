24791dc1ee66207cc473b5d298650dfe6a0b0cd6
valentinDev <valentin@MacBook-Pro-de-valentin.local>
2021-04-27 12:02:06 -0300

    Todo el proyecto de Mercado-Liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3001, () => console.log('Servicio en marcha'));
